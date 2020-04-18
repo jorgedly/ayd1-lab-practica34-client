4acc82a1ccf3a1a5abeaff3070bf28e46c50cda2
Ruth Nohemy Ardón Lechuga <ruthlechuga.1997@gmail.com>
2020-04-17 21:51:28 -1200

    estructura general

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,15 +7,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MaterialModule } from './material.module';
 import { LoginComponent } from './components/login/login.component';
-import { SigninComponent } from './components/signin/signin.component';
-import { IndexComponent } from './components/index/index.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    SigninComponent,
-    IndexComponent
   ],
   imports: [
     BrowserModule,
