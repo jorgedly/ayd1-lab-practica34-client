7c8b1bbea4305c866a88b63e898ff4816d9c5f9c
Mario Morales <mmario_93@hotmail.es>
2020-05-05 17:23:34 -0600

    Agregacion de generar reportes en pdf

--- src/app/components/reportes/reportes.component.ts
+++ src/app/components/reportes/reportes.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ReportesService } from '../../services/reportes.service';
+import pdfMake from 'pdfmake/build/pdfmake';
+import pdfFonts from 'pdfmake/build/vfs_fonts';
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 @Component({
   selector: 'app-reportes',
@@ -15,30 +18,159 @@ export class ReportesComponent implements OnInit {
 
   ngOnInit(): void {
     this.cta = localStorage.getItem('no_cuenta');
-    console.log(this.cta);
     this.obtenerGeneral();
     this.obtenerMis(this.cta);
   }
 
-  obtenerGeneral(){
+  obtenerGeneral() {
     this.reporteService.TranferenciaGeneral().subscribe(
-      res=> {
-        console.log(res);
+      res => {
         this.general = res;
-        console.log(this.general)
       },
       err => console.log(err)
     )
   }
 
-  obtenerMis(id){
+  obtenerMis(id) {
     this.reporteService.misTransferencias(id).subscribe(
       res => {
-        console.log(res);
         this.miReporte = res;
       },
       err => console.log(err)
     )
+  }
+
+  generatePdf() {
+    var encabezado = ['No', 'Cuenta_Origen', 'Cuenta Destino', 'No._Transaccion', 'Monto'];
+    var tableBody = [];
+
+    for (var i = 0; i < encabezado.length; i++) {
+      var row = [];
+      for (var j = 0; j < this.general.length; j++) {
+        if(i === 0) {
+          row[j] =j+1;
+        }
+        if(i === 1) {
+          row[j] =this.general[j].cuenta_origen;
+        }
+        if(i === 2) {
+          row[j] =this.general[j].cuenta_destino;
+        }
+        if(i === 3) {
+          row[j] =this.general[j].id_transaccion;
+        }
+        if(i === 4) {
+          row[j] =this.general[j].monto;
+        }
+       
+       
+      }
+      tableBody.push(row);
+      row = [];
+    }
+
+    const documentDefinition = {
+      content: [
+        {
+          text: 'Transferencias Generales',
+          bold: true,
+          fontSize: 20,
+          alignment: 'center',
+          margin: [0, 0, 0, 20]
+        },
+        {
+          table: {
+            headerRows: 1,
+            widths: ['*', '*', '*', '*', '*'],
+            body: [
+              ['No', 'Cuenta_Origen', 'Cuenta Destino', 'No._Transaccion', 'Monto'],
+              tableBody
+            ]
+          }
+        }
+      ], info: {
+        title: 'Analisis y Diseño 1',
+        author: 'Grupo No. 5',
+        subject: 'RESUME',
+        keywords: 'Transferencias Generales',
+      },
+    };
+    pdfMake.createPdf(documentDefinition).open();
+  }
+
+  generateMiPdf() {
+    var encabezado = ['No', 'Cuenta_Origen', 'Cuenta Destino', 'No._Transaccion', 'Monto'];
+    var tableBody = [];
+
+    for (var i = 0; i < encabezado.length; i++) {
+      var row = [];
+      for (var j = 0; j < this.miReporte.length; j++) {
+        if(i === 0) {
+          row[j] =j+1;
+        }
+        if(i === 1) {
+          row[j] =this.miReporte[j].cuenta_origen;
+        }
+        if(i === 2) {
+          row[j] =this.miReporte[j].cuenta_destino;
+        }
+        if(i === 3) {
+          row[j] =this.miReporte[j].id_transaccion;
+        }
+        if(i === 4) {
+          row[j] =this.miReporte[j].monto;
+        }
+       
+       
+      }
+      tableBody.push(row);
+      row = [];
+    }
+
+    const documentDefinition = {
+      content: [
+        {
+          text: 'Mis Transferencias',
+          bold: true,
+          fontSize: 20,
+          alignment: 'center',
+          margin: [0, 0, 0, 20]
+        },
+        {
+          columns: [
+            [{
+              text: localStorage.getItem('nombres'),
+              style: 'name'
+            },
+            {
+              text: localStorage.getItem('apellidos'),
+              style: 'name'
+            },
+            {
+              text: 'Cuenta No.: ' + localStorage.getItem('no_cuenta'),
+            },
+            ]
+          ]
+        },
+      
+        {
+          table: {
+            headerRows: 1,
+            widths: ['*', '*', '*', '*', '*'],
+            body: [
+              ['No', 'Cuenta_Origen', 'Cuenta Destino', 'No._Transaccion', 'Monto'],
+              tableBody
+            ]
+          }
+        }
+      ], info: {
+        title: 'Analisis y Diseño 1',
+        author: 'Grupo No. 5',
+        subject: 'RESUME',
+        keywords: 'Transferencias Generales',
+      },
+    };
+    pdfMake.createPdf(documentDefinition).open();
   }
 
 
