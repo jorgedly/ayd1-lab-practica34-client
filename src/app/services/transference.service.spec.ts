0b365244413dd93e066a36ea7a0c07a09a900170
HyperX98 <raulxiloj98@gmail.com>
2020-05-06 21:19:13 -0600

    Unit testing of transference module

--- src/app/services/transference.service.spec.ts
+++ src/app/services/transference.service.spec.ts
@@ -1,9 +1,11 @@
-import { TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { TestBed, getTestBed, inject } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { TransferenceService } from './transference.service';
 
 describe('TransferenceService', () => {
+  let injector: TestBed;
+  let httpMock: HttpTestingController;
   let service: TransferenceService;
 
   beforeEach(() => {
@@ -11,10 +13,27 @@ describe('TransferenceService', () => {
       imports: [HttpClientTestingModule],
       providers: [TransferenceService]
     });
+    injector = getTestBed();
+    httpMock = injector.get(HttpTestingController);
     service = TestBed.inject(TransferenceService);
   });
 
-  // it('should be created', () => {
-  //   expect(service).toBeTruthy();
-  // });
+
+  it('should post the transaction', () => {
+    const service: TransferenceService = TestBed.get(TransferenceService);
+    let cuenta1 = 8;
+    let cuenta2 = 9;
+    let monto = 500; 
+    const dummy = {res: true}
+
+    service.transferMoney(cuenta1, cuenta2, monto).subscribe( res => {
+      expect(res).toEqual(dummy);
+    });
+
+    const req = httpMock.expectOne('https://analisis1.herokuapp.com/transferMoney')
+    expect(req.request.method).toBe('POST')
+    req.flush(dummy);
+
+  });
+
 });
